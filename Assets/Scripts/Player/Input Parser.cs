208cca126d21b90bc0023acb17504e1cbd30856c
MartijnRene <martijnrenevdmeer@gmail.com>
2024-03-21 14:52:32 +0100

    Added new input actions and input parser

---
+++ Assets/Scripts/Player/Input Parser.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using FrameWork;
+
+namespace Player.Input_Parser
+{
+    public sealed class InputParser : MonoBehaviour
+    {
+        private const string INTERACTABLE_TAG = "Interactable";
+        
+        [SerializeField, Range(10, 100)] private float interactableRayDistance;
+        
+        private Camera _mainCamera;
+        private PlayerInput _playerInput;
+        private InputActionAsset _inputActionAsset;
+        private InteractableObject _lastInteractable;
+        
+        private void Awake()
+        {
+            GetReferences();
+            Init();
+        }
+
+        private void OnEnable() => AddListeners();
+
+        private void OnDisable() => RemoveListeners();
+        
+        private void GetReferences()
+        {
+            _playerInput = GetComponent<PlayerInput>();
+        }
+
+        private void Init()
+        {
+            _inputActionAsset = _playerInput.actions;
+            _mainCamera = Camera.main;
+        }
+
+        private void AddListeners()
+        {
+            _inputActionAsset["Interact"].performed += Interact;
+        }
+
+        private void RemoveListeners()
+        {
+            _inputActionAsset["Interact"].performed -= Interact;
+        }
+
+        private void Interact(InputAction.CallbackContext context)
+        {
+            Ray ray = _mainCamera.ScreenPointToRay(GetMousePosition());
+            Physics.Raycast(ray.origin, ray.direction * interactableRayDistance, out RaycastHit hit);
+
+            if (!hit.collider
+                || !hit.collider.CompareTag(INTERACTABLE_TAG))
+                return;
+            
+            if (_lastInteractable == null
+                || _lastInteractable.transform != hit.collider.transform)
+                _lastInteractable = hit.collider.GetComponent<InteractableObject>();
+                
+            _lastInteractable.onInteract?.Invoke();
+        }
+
+        private Vector3 GetMousePosition() => (Vector3) _inputActionAsset["MousePosition"].ReadValue<Vector2>();
+    }
+}

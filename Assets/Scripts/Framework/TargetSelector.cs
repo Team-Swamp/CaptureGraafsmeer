725cefa8eb46a17ff43343bb3e0cc13ea447190a
Wildeaso1 <70636740+Wildeaso1@users.noreply.github.com>
2024-03-28 14:33:48 +0100

    line renders from player to object

--- Assets/Scripts/Framework/TargetSelector.cs
+++ Assets/Scripts/Framework/TargetSelector.cs
@@ -5,19 +5,18 @@ namespace Framework
 {
     public class TargetSelector : MonoBehaviour
     {   
-        public List<Transform> targetList;
-        public LineRenderer lineRenderer;
-        public Transform currentTarget;
-        public float closeRange = 5.0f;
+        [SerializeField] private List<Transform> targetList;
+        [SerializeField] private LineRenderer lineRenderer;
+        [SerializeField] private Transform currentTarget;
+        [SerializeField] private float closeRange = 5.0f;
 
         private int currentTargetIndex;
 
         private void Start()
         {
-            print("test");
             currentTargetIndex = 0;
             currentTarget = targetList[1];
-            // Initialize the line renderer and the first target
+            
             lineRenderer.positionCount = 2;
             lineRenderer.SetPosition(0, transform.position);
             lineRenderer.SetPosition(1, currentTarget.position);
@@ -29,7 +28,6 @@ namespace Framework
             lineRenderer.SetPosition(0, transform.position);
             if (Vector3.Distance(transform.position, currentTarget.position) < closeRange)
             {
-                // Select the next target in the list
                 currentTargetIndex = (currentTargetIndex + 1) % targetList.Count;
                 currentTarget = targetList[currentTargetIndex];
                 lineRenderer.SetPosition(1, currentTarget.position);

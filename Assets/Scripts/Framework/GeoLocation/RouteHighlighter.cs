72a0cee600672e59554f9f46e266e094d1c61f8d
Wildeaso1 <70636740+Wildeaso1@users.noreply.github.com>
2024-04-02 15:37:36 +0200

    Code review

--- Assets/Scripts/Framework/GeoLocation/RouteHighlighter.cs
+++ Assets/Scripts/Framework/GeoLocation/RouteHighlighter.cs
@@ -1,37 +1,69 @@
 using System;
 using System.Collections.Generic;
-using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Framework.GeoLocation
 {
+    [RequireComponent(typeof(LineRenderer))]
     public sealed class RouteHighlighter : MonoBehaviour
-    {   
+    {
+        private const float INVOKE_DELAY = 0.1f;
+
+        [SerializeField] private UnityEvent withinRange = new UnityEvent();
+        
         [SerializeField] private List<Transform> routePoints;
         [SerializeField] private LineRenderer lineRenderer;
-
-        private int _routeIndex;
+        [SerializeField] private float closeRange = 5f;
+        
+        [SerializeField] private Transform _nextPoint;
 
         private void Start()
         {
-            Invoke("InitLines", 0.1f);
+            Invoke(nameof(InitLine), INVOKE_DELAY);
+            _nextPoint = routePoints[1];
+            
         }
 
-        private void InitLines()
+        private void Update()
+        {
+            if (Vector3.Distance(transform.position, _nextPoint.position) < closeRange)
+            {
+                withinRange?.Invoke();
+            }
+        }
+
+        public void RemoveLine()
+        {
+            if (routePoints.Count <= 2)
+            {
+                lineRenderer.enabled = false;
+                routePoints.Clear();
+            }
+            else
+            {
+                _nextPoint = routePoints[2];
+                routePoints.RemoveAt(0);
+                lineRenderer.positionCount = 0;
+                InitLine();
+            }
+
+        }
+        private void InitLine()
         {
             try
             {
                 foreach (var point in routePoints)
                 {
                     lineRenderer.positionCount++;
-                    _routeIndex = lineRenderer.positionCount - 1;
-                    lineRenderer.SetPosition(_routeIndex, point.position);
+                    int index = lineRenderer.positionCount - 1;
+                    lineRenderer.SetPosition(index, point.position);
+                    print(point);
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
-                throw;
+                throw new Exception(e.ToString());
             }
         }
     }

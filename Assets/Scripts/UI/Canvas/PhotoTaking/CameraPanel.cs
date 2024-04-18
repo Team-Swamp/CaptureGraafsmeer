3f52262fd11e58d98f785588907ffa4af2c2a0a6
MartijnRene <martijnrenevdmeer@gmail.com>
2024-04-18 13:34:18 +0200

    applied second feedback

--- Assets/Scripts/UI/Canvas/PhotoTaking/CameraPanel.cs
+++ Assets/Scripts/UI/Canvas/PhotoTaking/CameraPanel.cs
@@ -2,21 +2,22 @@ using UnityEngine;
 using TMPro;
 
 using Framework.ScriptableObjects;
+using UnityEngine.Serialization;
 
 namespace UI.Canvas.PhotoTaking
 {
     public sealed class CameraPanel : MonoBehaviour
     {
-        [SerializeField] private TMP_Text buildingName;
-        [SerializeField] private Texture2D buildingRender;
+        [SerializeField] private TMP_Text pointOfInterestName;
+        [SerializeField] private Texture2D pointOfInterestRender;
 
         /// <summary>
         /// Fills the camera panel with data from the given scriptable object
         /// </summary>
         /// <param name="photoData"> The scriptable object used to get data </param>
-        public void FillPanelData(PhotoData photoData)
+        public void SetPanelData(PhotoData photoData)
         {
-            buildingName.text = photoData.Title;
+            pointOfInterestName.text = photoData.Title;
             //to do: set image from scriptable object
         }
     }   

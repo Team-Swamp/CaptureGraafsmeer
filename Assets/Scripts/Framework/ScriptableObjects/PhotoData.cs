36f1be913d68b4f467bd0e7d2172726bb0d840ab
bas-boop <dereusbas461@gmail.com>
2024-05-06 09:41:20 +0200

    Added renders to photodata

--- Assets/Scripts/Framework/ScriptableObjects/PhotoData.cs
+++ Assets/Scripts/Framework/ScriptableObjects/PhotoData.cs
@@ -6,11 +6,15 @@ namespace Framework.ScriptableObjects
     public sealed class PhotoData : ScriptableObject
     {
         private const string TAKE_PHOTO = "Maak een foto om hier over te leren.";
-        
+
+        [SerializeField] private Texture render;
         [SerializeField] private string title = "Object title";
         [SerializeField, TextArea(3, 6)] private string info ;
 
+        public Texture Render => render;
+        
         public (string, string) Info => (info, TAKE_PHOTO);
+        
         public string Title => title;
     }
 }

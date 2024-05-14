a11c5644189e71a19320f43b751b8240f2cdfced
bas-boop <dereusbas461@gmail.com>
2024-05-14 10:45:11 +0200

    Tweaked CoordinatesTransform to have a scale factor

---
+++ Assets/Editor/CoordinatesTransformEditor.cs
@@ -0,0 +1,56 @@
+﻿using UnityEditor;
+
+using Framework.GeoLocation;
+
+namespace Editor
+{
+    [CustomEditor(typeof(CoordinatesTransform))]
+    public sealed class CoordinatesTransformEditor : UnityEditor.Editor
+    {
+        private SerializedProperty _cords;
+        private SerializedProperty _scale;
+        private SerializedProperty _isStatic;
+        private SerializedProperty _isDebugTesting;
+        private SerializedProperty _isPlayer;
+        private SerializedProperty _lerp;
+        private SerializedProperty _others;
+
+        /// <summary>
+        /// Display every serialized variable, headers and space.
+        /// </summary>
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+    
+            EditorGUILayout.LabelField("Settings", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(_cords);
+            EditorGUILayout.PropertyField(_scale);
+            EditorGUILayout.PropertyField(_isStatic);
+            
+            if (_isStatic.boolValue)
+                EditorGUILayout.PropertyField(_isDebugTesting);
+            
+            EditorGUILayout.PropertyField(_isPlayer);
+
+            if (_isPlayer.boolValue)
+            {
+                EditorGUILayout.LabelField("Player settings", EditorStyles.boldLabel);
+                EditorGUILayout.PropertyField(_lerp);
+                EditorGUILayout.PropertyField(_others);
+            }
+            
+            serializedObject.ApplyModifiedProperties();
+        }
+        
+        private void OnEnable()
+        {
+            _cords = serializedObject.FindProperty("coordinates");
+            _scale = serializedObject.FindProperty("scaleFactor");
+            _isStatic = serializedObject.FindProperty("isStatic");
+            _isPlayer = serializedObject.FindProperty("isPlayer");
+            _isDebugTesting = serializedObject.FindProperty("isDebugTesting");
+            _lerp = serializedObject.FindProperty("lerpTime");
+            _others = serializedObject.FindProperty("others");
+        }
+    }
+}
--- Assets/Scripts/Framework/GeoLocation/CoordinatesTransform.cs
+++ Assets/Scripts/Framework/GeoLocation/CoordinatesTransform.cs
@@ -12,17 +12,21 @@ namespace Framework.GeoLocation
         private const int HALF_CIRCLE = 180;
         private const int EARTH_RADIUS = 6378137;
         private const string PLAYER_STATIC_ERROR = "The player is not a static CoordinatesTransform!";
+        private const string PLAYER_NO_OTHERS_ERROR = "The player needs to refrence other CoordinatesTransform! At least 2 are needed.";
         
         private static readonly Vector2 origin = new (52.356531f, 4.9308f);
-
+        
         [SerializeField] private Vector2 coordinates;
+        [SerializeField] private Vector2 scaleFactor = Vector2.one;
         [SerializeField] private bool isStatic;
         [SerializeField] private bool isPlayer;
-        [SerializeField, Range(1, 25)] private float lerpTime;
+        [SerializeField] private bool isDebugTesting;
+        [SerializeField, Range(1, 25)] private float lerpTime = 2.5f;
+        [SerializeField] private CoordinatesTransform[] others;
 
         private LocationUpdater _player;
         private bool _isReactive;
-
+        
         private void Awake()
         {
             if (isPlayer)
@@ -43,10 +47,18 @@ namespace Framework.GeoLocation
             
             if (isStatic)
                 UpdateLocation(null);
+
+            if (isPlayer
+                && others.Length == 0
+                || others.Length <= 2)
+                throw new Exception(PLAYER_NO_OTHERS_ERROR);
         }
 
         private void Update()
         {
+            if (isDebugTesting)
+                UpdateLocation(null);
+            
             if(isStatic)
                 return;
             
@@ -64,7 +76,9 @@ namespace Framework.GeoLocation
             Vector2 targetPosition = pos ?? new Vector2(coordinates.x, coordinates.y);
             targetPosition.Subtract(origin);
             (double latitude, double longitude) = ConvertToMeters(targetPosition.x, -targetPosition.y);
-            Vector3 finalTargetPosition = new Vector3((float)latitude, 0, (float)longitude);
+            Vector3 finalTargetPosition = isPlayer
+                ? BlendPlayerPosition(latitude, longitude)
+                : new Vector3((float)latitude, 0, (float)longitude);
 
             if (_isReactive)
                 return;
@@ -74,10 +88,48 @@ namespace Framework.GeoLocation
 
         private (double, double) ConvertToMeters(double latitude, double longitude)
         {
-            double latitudeInMeters = latitude * Math.PI / HALF_CIRCLE * EARTH_RADIUS;
-            double longitudeInMeters = longitude * Math.PI / HALF_CIRCLE * EARTH_RADIUS;
+            double latitudeInMeters = latitude * Math.PI / HALF_CIRCLE * EARTH_RADIUS * scaleFactor.x;
+            double longitudeInMeters = longitude * Math.PI / HALF_CIRCLE * EARTH_RADIUS * scaleFactor.y;
 
             return (latitudeInMeters, longitudeInMeters);
+        }
+
+        private Vector3 BlendPlayerPosition(double latitude, double longitude)
+        {
+            (CoordinatesTransform closeted, CoordinatesTransform secondCloseted) = FindTwoClosestGameObjects();
+
+            scaleFactor = scaleFactor.Multiply(closeted.scaleFactor)
+                .Midpoint(secondCloseted.scaleFactor);
+            Vector3 finalPosition = new ((float) latitude, 0, (float) longitude);
+            finalPosition.Multiply(scaleFactor);
+            
+            return finalPosition;
+        }
+        
+        private (CoordinatesTransform, CoordinatesTransform) FindTwoClosestGameObjects()
+        {
+            CoordinatesTransform closest1 = null;
+            CoordinatesTransform closest2 = null;
+            float shortestDistance = float.MaxValue;
+
+            foreach (var currentOtherTransform in others)
+            {
+                float distance = Vector3.Distance(currentOtherTransform.gameObject.transform.position, transform.position);
+                
+                if (distance < shortestDistance)
+                {
+                    closest2 = closest1;
+                    closest1 = currentOtherTransform;
+                    shortestDistance = distance;
+                }
+                else if (closest2 == null
+                         || distance < Vector3.Distance(closest2.transform.position, transform.position))
+                {
+                    closest2 = currentOtherTransform;
+                }
+            }
+
+            return (closest1, closest2);
         }
         
         private IEnumerator LerpPosition(Vector3 targetPosition)
